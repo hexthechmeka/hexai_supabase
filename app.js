83847631a9a8b4ca817407f9a55f2d81578c215b
hexthechmeka <hexthechmeka@gmail.com>
2025-06-23 22:55:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-// app.js: Express 앱 설정
-
 const express = require('express')
 const app = express()
 const usageRoutes = require('./routes/usageRoutes')
 const loginRoutes = require('./routes/loginRoutes')
 
-// JSON 요청 본문 파싱
 app.use(express.json())
-app.use('/login', loginRoutes)
 
-// 라우터 연결
+// 루트 응답
+app.get('/', (req, res) => {
+  res.send('HexAI API 서버 실행 중');
+});
+
+app.use('/login', loginRoutes)
 app.use('/', usageRoutes)
 
 module.exports = app
