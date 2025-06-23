7f5cc7b895ea14ae331c16bb98644e6e1468b957
hexthechmeka <hexthechmeka@gmail.com>
2025-06-23 23:48:49 +0900

    fix

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const app = express()
-const usageRoutes = require('./routes/usageRoutes')
-const loginRoutes = require('./routes/loginRoutes')
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());  // body 파서를 모든 라우트보다 위에 적용
+
+const usageRoutes = require('./routes/usageRoutes');
+const loginRoutes = require('./routes/loginRoutes');
+const gptRoutes = require('./routes/gptRoutes');
+
+app.use('/api', gptRoutes);
+app.use('/login', loginRoutes);
+app.use('/usage', usageRoutes);
 
 app.get('/', (req, res) => {
   res.send('HexAI API 서버 실행 중');
 });
 
-app.use('/login', loginRoutes)
-app.use('/usage', usageRoutes)
-
-module.exports = app
+module.exports = app;
