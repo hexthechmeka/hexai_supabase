ed54cbf4695965ac9fe35fdb15b1a216f783b9a0
hexthechmeka <hexthechmeka@gmail.com>
2025-06-23 22:56:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const loginRoutes = require('./routes/loginRoutes')
 
 app.use(express.json())
 
-// 루트 응답
 app.get('/', (req, res) => {
   res.send('HexAI API 서버 실행 중');
 });
 
 app.use('/login', loginRoutes)
-app.use('/', usageRoutes)
+app.use('/usage', usageRoutes)
 
 module.exports = app
